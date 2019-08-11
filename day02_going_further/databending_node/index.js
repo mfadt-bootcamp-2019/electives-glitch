f0bb8576c685a8d5a510aaa3672032d052c535b4
skiprox <seanmichaelscanlan@gmail.com>
2019-08-11 15:52:32 -0400

    Updating with databending in node

---
+++ day02_going_further/databending_node/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Require FS, which is node filesystem
+const fs = require('fs');
+
+class DataBend {
+	constructor(path, fileName) {
+		// Make the output directory if it isn't there yet
+		if (!fs.existsSync('output')){
+			fs.mkdirSync('output');
+		}
+		fs.readFile(`${path}/${fileName}`, (err, buf) => {
+			if (err) throw err;
+			for (let i = 0; i < 100; i++) {
+				let ranByte1 = Math.floor(Math.random() * buf.length);
+				let ranByte2 = Math.floor(Math.random() * buf.length);
+				buf[ranByte1] = buf[ranByte2];
+			}
+			fs.writeFileSync(`output/${fileName}`, buf, (err) => {
+				if (err) throw err;
+			});
+		});
+	}
+}
+
+new DataBend('img', 'bob.jpg');
